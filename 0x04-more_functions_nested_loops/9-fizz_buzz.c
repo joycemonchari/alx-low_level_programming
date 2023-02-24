25ffb93193b654431251ed91bbd072f1cd2f1c13
Monchari <moncharijoyce@gmail.com>
2023-02-23 18:06:13 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,10 +4,10 @@
  * main - entry point for the program
  *
  * This function prints the numbers from 1 to 100, replacing multiples of 3
- * with "Fizz", multiples of 5 with "Buzz",
- * and multiples of both 3 and 5 with "FizzBuzz"
+ * with Fizz, multiples of 5 with Buzz
+ * and multiples of both 3 and 5 with FizzBuzz
  *
- * Return: void
+ * Return: Always 0 (success)
  */
 
 int main(void)
