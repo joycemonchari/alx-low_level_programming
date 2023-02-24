fc6665970679aabc0cef6e5f4b763c210d4bbb25
Monchari <moncharijoyce@gmail.com>
2023-02-23 17:35:14 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,7 +5,7 @@
  *
  * This function prints the numbers from 1 to 100, replacing multiples of 3
  * with "Fizz", multiples of 5 with "Buzz",
- * and multiples of both 3 and 5 with "FizzBuzz".
+ * and multiples of both 3 and 5 with "FizzBuzz"
  *
  * Return: Always 0 (Success)
  */
