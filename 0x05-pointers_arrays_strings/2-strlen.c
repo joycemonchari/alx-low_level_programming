a5b323e49f036ad5559f806b27641a1775d035b3
Monchari <moncharijoyce@gmail.com>
2023-02-27 11:38:28 -0800

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -18,14 +18,3 @@ int _strlen(char *s)
 
 	return (length);
 }
-
-int main(void)
-{
-	char *str;
-	int len;
-
-	str = "My first strlen!";
-	len = _strlen(str);
-	printf("%d\n", len);
-	return (0);
-}
