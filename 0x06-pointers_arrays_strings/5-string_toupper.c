4d3ff1226c7b38d2d39b14720fa560329dea3593
Monchari <moncharijoyce@gmail.com>
2023-03-02 06:54:39 -0800

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -19,3 +19,13 @@ char *string_toupper(char *str)
 	}
 	return (str);
 }
+int main(void)
+{
+	char str[] = "Look up!\n";
+	char *ptr;
+
+	ptr = string_toupper(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
+}
