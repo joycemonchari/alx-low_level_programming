6e83850b95e90719800b1a226c8e72d3d8a07a3d
Monchari <moncharijoyce@gmail.com>
2023-03-02 05:08:11 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,7 +7,7 @@
  * @src: input value
  * Return: void
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
@@ -25,6 +25,6 @@ char *_strcat(char *dest, char *src);
 		j++;
 	}
 
-	dest[i] != '\0'
+	dest[i] = '\0'
 	return (dest);
 }
