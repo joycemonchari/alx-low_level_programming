36998399d4abf8b0a0b45a3496298fd44da55341
Monchari <moncharijoyce@gmail.com>
2023-03-09 15:02:32 -0800

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,35 +1,38 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number
- * @n: The number to find the square root of
+ * _sqrt_recursion - Returns the natural square root of a number.
  *
- * Return: The natural square root of n or -1 if n
- * does not have a natural square root
+ * @n: The number to find the natural square root of.
+ *
+ * Return: The natural square root of n, or -1 if n does not have
+ *         a natural square root.
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
 
-	return (sqrt_helper(n, 1));
+	return (find_sqrt(n, 1));
 }
 
 /**
- * sqrt_helper - Recursive helper function for _sqrt_recursion
- * @n: The number to find the square root of
- * @i: The current guess for the square root
+ * find_sqrt - Finds the square root of a number using recursion.
  *
- * Return: The natural square root of n or -1 if n
- * does not have a natural square root
+ * @n: The number to find the square root of.
+ * @i: The current guess for the square root.
+ *
+ * Return: The square root of n.
  */
-int sqrt_helper(int n, int i)
+int find_sqrt(int n, int i)
 {
+	if (i * i == n)
+		return (i);
+
 	if (i * i > n)
 		return (-1);
-	else if (i * i == n)
-		return (i);
-	else
-		return (sqrt_helper(n, i + 1));
+
+	return (find_sqrt(n, i + 1));
 }
